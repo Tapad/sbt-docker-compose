f64fee2e45bd9218aafd28713954ee15ab0f3e23
Kurt Kopchik <kurt.kopchik@tapad.com>
2016-02-03 13:50:00 -0500

    Sample test cleanup

--- examples/basic-with-tests/src/main/scala/BasicApp.scala
+++ examples/basic-with-tests/src/main/scala/BasicApp.scala
@@ -1,9 +1,8 @@
-import scala.Console._
 import java.io.PrintWriter
 import java.net.ServerSocket
 
 object BasicApp extends App {
-  
+
   val text =
     """HTTP/1.0 200 OK
         Content-Type: text/html
@@ -12,7 +11,6 @@ object BasicApp extends App {
         <HTML> <HEAD> <TITLE>Hello, World!</TITLE> </HEAD> <BODY LANG="en-US" BGCOLOR="#e6e6ff" DIR="LTR"> <P ALIGN="CENTER"> <FONT FACE="Arial, sans-serif" SIZE="6">Hello, World!</FONT> </P> </BODY> </HTML>"""
   val port = 8080
   val listener = new ServerSocket(port)
-  printf("Listening at port %1$d", port)
 
   while (true) {
     val sock = listener.accept()
