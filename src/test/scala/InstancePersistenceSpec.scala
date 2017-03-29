7b7c5a10e5e29e58247e954b610b9c4f3e6509e1
Mark Canlas <mark.canlas@tapad.com>
2017-03-29 17:18:49 -0400

    avoid deprecation warnings

--- src/test/scala/InstancePersistenceSpec.scala
+++ src/test/scala/InstancePersistenceSpec.scala
@@ -1,7 +1,7 @@
 import com.tapad.docker.{ RunningInstanceInfo, DockerComposePluginLocal }
 import com.tapad.docker.DockerComposeKeys._
 import org.mockito.Mockito._
-import org.scalatest.mock.MockitoSugar
+import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{ BeforeAndAfter, FunSuite, OneInstancePerTest }
 
 class InstancePersistenceSpec extends FunSuite with BeforeAndAfter with OneInstancePerTest with MockitoSugar {
