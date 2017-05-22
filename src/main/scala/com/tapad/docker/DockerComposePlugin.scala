5b61e0cc506eeeca312e6a598244978c319661f6
Kurt Kopchik <kurt.kopchik@tapad.com>
2017-05-21 21:43:53 -0400

    Update 'Docker for Mac' detection text to specify that this can also be 'Docker for Windows' as well.

--- src/main/scala/com/tapad/docker/DockerComposePlugin.scala
+++ src/main/scala/com/tapad/docker/DockerComposePlugin.scala
@@ -473,7 +473,7 @@ class DockerComposePluginLocal extends AutoPlugin with ComposeFile with DockerCo
       print("OSX boot2docker environment detected. Using the docker-machine IP for the container.")
       dockerMachineIp(dockerMachineName)
     } else if (isDockerForMacEnvironment) {
-      print("Docker for Mac environment detected. Using the localhost for the container.")
+      print("Docker for Mac/Windows environment detected. Using the localhost for the container.")
       "localhost"
     } else {
       print("Non-OSX environment detected. Using the host from the container.")
